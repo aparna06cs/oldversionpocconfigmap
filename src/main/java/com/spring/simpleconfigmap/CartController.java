6ab0cdb9d0014dec423a7d69ecb92e0f8d8cd7e9
aparna <aparna.06cs03@gmail.com>
2022-07-04 17:12:06 +0530

    changes

--- src/main/java/com/spring/simpleconfigmap/CartController.java
+++ src/main/java/com/spring/simpleconfigmap/CartController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.configmap.service.CartService;
+
 @RefreshScope
 @RequestMapping("/cart")
 @RestController
 public class CartController {
 	
+	/*
+	 * @Autowired private CartConfiguration cartconf;
+	 */
+	
 	@Autowired
-	private CartConfiguration cartconf;
+	private CartService cartService;
 
 	@Value("${cart.success:property from spring boot local for 1.5}")
 	private String cartSucess;
@@ -39,59 +45,45 @@ public class CartController {
 	@Value("${failure1:property from spring boot local for 1.5}")
 	private String failure1;
 	
-	@GetMapping("conf/cartsuccess")
-    public String cartSuccessmethod() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getCartSucess());
-        return cartconf.getCartSucess();
-    }
-	
-	@GetMapping("conf/cartsuccess1")
-    public String cartSuccessmethod1() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getCartSucess1());
-        return cartconf.getCartSucess1();
-    }
-	
-	@GetMapping("conf/cartfailure")
-    public String cartfailuremethod() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getCartFailure());
-        return cartconf.getCartFailure();
-    }
-	
-	@GetMapping("conf/cartfailure1")
-    public String cartfailuremethod1() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getFailure1());
-        return cartconf.getFailure1();
-    }
-	
-	@GetMapping("conf/success")
-    public String cartSuccessmethod3() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getSuccess());
-        return cartconf.getSuccess();
-    }
-	
-	@GetMapping("conf/success1")
-    public String cartSuccessmethod4() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getSuccess1());
-        return cartconf.getSuccess1();
-    }
-	@GetMapping("conf/failure")
-    public String cartfailuremethod3() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getFailure());
-        return cartconf.getFailure();
-    }
-	
-	@GetMapping("conf/failure1")
-    public String cartfailuremethod4() {
-        System.out.println("the controoler class config map 2 for 1.5->"+cartconf.getFailure1());
-        return cartconf.getFailure1();
-    }
-	
-	//from controller
-	@GetMapping("cartsuccess")
-    public String cartSuccessmethod11() {
-        System.out.println("the controoler class config map 2 for 1.5 from contr->"+cartSucess);
-        return cartconf.getCartSucess();
-    }
+	/*
+	 * @GetMapping("conf/cartsuccess") public String cartSuccessmethod() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getCartSucess()); return cartconf.getCartSucess(); }
+	 * 
+	 * @GetMapping("conf/cartsuccess1") public String cartSuccessmethod1() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getCartSucess1()); return cartconf.getCartSucess1(); }
+	 * 
+	 * @GetMapping("conf/cartfailure") public String cartfailuremethod() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getCartFailure()); return cartconf.getCartFailure(); }
+	 * 
+	 * @GetMapping("conf/cartfailure1") public String cartfailuremethod1() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getFailure1()); return cartconf.getFailure1(); }
+	 * 
+	 * @GetMapping("conf/success") public String cartSuccessmethod3() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getSuccess()); return cartconf.getSuccess(); }
+	 * 
+	 * @GetMapping("conf/success1") public String cartSuccessmethod4() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getSuccess1()); return cartconf.getSuccess1(); }
+	 * 
+	 * @GetMapping("conf/failure") public String cartfailuremethod3() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getFailure()); return cartconf.getFailure(); }
+	 * 
+	 * @GetMapping("conf/failure1") public String cartfailuremethod4() {
+	 * System.out.println("the controoler class config map 2 for 1.5->"+cartconf.
+	 * getFailure1()); return cartconf.getFailure1(); }
+	 * 
+	 * //from controller
+	 * 
+	 * @GetMapping("cartsuccess") public String cartSuccessmethod11() {
+	 * System.out.println("the controoler class config map 2 for 1.5 from contr->"
+	 * +cartSucess); return cartconf.getCartSucess(); }
+	 */
 	
 	@GetMapping("cartsuccess1")
     public String cartSuccessmethod12() {
@@ -133,4 +125,11 @@ public class CartController {
         System.out.println("the controoler class config map 2 for 1.5- from contr>"+failure1);
         return failure1;
     }
+	
+	@GetMapping("cartsuccessFromService")
+    public String cartSuccessmethodfromService() {
+        System.out.println("the controoler class config map 2 for 1.5- from contr >"+cartSucess1);
+        return cartService.getcartMessage();
+    }
+	
 }
